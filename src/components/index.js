7b657fc5601a772ff1fe3c19a7fb1af75c2e2546
Vaibhav092 <vaibhavcr7092@gmail.com>
2024-11-13 23:25:56 +0530

    Adding pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,28 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from './container/Container.jsx'
 import Logo from "./Logo.jsx";
-import LogoutBtn from "./Header/LogoutBtn.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx"
+import RTE from "./RTE.jsx"
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
